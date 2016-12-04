5b5c2b869c2426137a52e02eddee2d2d77da3c62
sidsoft2014 <sidsoft2014@outlook.com>
2016-12-04 14:54:42 +0000

    add line break

--- LongPathKiller/Program.cs
+++ LongPathKiller/Program.cs
@@ -16,6 +16,7 @@ namespace LongPathKiller
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
         }
     }
